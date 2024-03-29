6a236cb9fcacbb5ed54cdfb6a55d0b2c947b48b6
Alvaro Royo <alvaroroyo3@gmail.com>
2024-03-29 12:10:39 +0100

    RoundedCorner

---
+++ Sources/ARCoreKit/Extensions/RoundedCorner.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+public struct RoundedCorner: Shape {
+    public var radius: CGFloat
+    public var corners: UIRectCorner
+    
+    public init(radius: CGFloat = .infinity, corners: UIRectCorner = .allCorners) {
+        self.radius = radius
+        self.corners = corners
+    }
+    
+    public func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
