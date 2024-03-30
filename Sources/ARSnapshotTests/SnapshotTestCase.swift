2caf40c1b572d670120361114cb0c3fd45a0a57f
Alvaro Royo <alvaroroyo3@gmail.com>
2024-03-30 22:48:50 +0100

    Added wait

--- Sources/ARSnapshotTests/SnapshotTestCase.swift
+++ Sources/ARSnapshotTests/SnapshotTestCase.swift
@@ -9,12 +9,12 @@ enum SnapshotError: Error {
 public extension XCTestCase {
 
     @MainActor
-    func verify<T: View>(view: T, record: Bool = false, testName: String = #function, file: StaticString = #file) throws {
-        try verify(viewController: UIHostingController(rootView: view.ignoresSafeArea(edges: .all)), record: record, testName: testName, file: file)
+    func verify<T: View>(view: T, wait: TimeInterval = 0, record: Bool = false, testName: String = #function, file: StaticString = #file) throws {
+        try verify(viewController: UIHostingController(rootView: view.ignoresSafeArea(edges: .all)), wait: wait, record: record, testName: testName, file: file)
     }
 
     @MainActor
-    func verify<T: UIViewController>(viewController: T, record: Bool = false, testName: String = #function, file: StaticString = #file) throws {
+    func verify<T: UIViewController>(viewController: T, wait: TimeInterval = 0, record: Bool = false, testName: String = #function, file: StaticString = #file) throws {
 
         let fileUrl = URL(fileURLWithPath: "\(file)", isDirectory: false)
         let fileName = fileUrl.deletingPathExtension().lastPathComponent
@@ -27,11 +27,13 @@ public extension XCTestCase {
 
         if let message = verifySnapshot(
             of: viewController,
-            as: .image(
-                precision: 0.97,
-                perceptualPrecision: 0.97,
-                size: CGSize(width: 400, height: 800),
-                traits: .init(displayScale: 1.0)
+            as: .wait(for: wait, on:
+                    .image(
+                        precision: 0.97,
+                        perceptualPrecision: 0.97,
+                        size: CGSize(width: 400, height: 800),
+                        traits: .init(displayScale: 1.0)
+                     )
             ),
             snapshotDirectory: snapshotDirectoryUrl.path,
             testName: testName
